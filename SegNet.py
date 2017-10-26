1c9f6c4504a8a1dbe67ab9dbc64a42d72d22ea24
ykamikawa <ytkmkw@gmail.com>
2017-10-26 23:11:29 +0900

    Complete Model imlementation

--- SegNet.py
+++ SegNet.py
@@ -1,17 +1,17 @@
 from keras.models import Model
-from keras.layers.core import Input, Layer, Dense, Dropout, Activation, Flatten, Reshape, Merge, Permute
+from keras.layers import Input
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape, Permute
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
 from keras.layers.normalization import BatchNormalization
+from keras.layers.merge import Multiply, Concatenate
 from keras.utils import np_utils
-from keras.regularizers import ActivityRegularizer
-from keras.utils.visualize_util import plot
 
-from .keras_IndicesPooling import UnPooling2DIndices, MaxPoolingMask2D
+from keras_IndicesPooling import MaxPoolingMask2D
 
 import os
 import numpy as np
 import argparse
-import hdf5
+import json
 from PIL import Image
 
 def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
@@ -21,9 +21,9 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
 
     conv_1 = Convolution2D(64, (kernel, kernel), padding="same")(inputs)
     conv_1 = BatchNormalization()(conv_1)
-    conv_1 = Activation("relu")(conv_2)
-    conv_2 = Convolution2D(64, (kernel, kernel), padding="same")(conv1)
-    conv_2 = BatchNormalization()(conv_1)
+    conv_1 = Activation("relu")(conv_1)
+    conv_2 = Convolution2D(64, (kernel, kernel), padding="same")(conv_1)
+    conv_2 = BatchNormalization()(conv_2)
     conv_2 = Activation("relu")(conv_2)
 
     pool_1 = MaxPooling2D(pool_size)(conv_2)
@@ -39,117 +39,125 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     pool_2 = MaxPooling2D(pool_size)(conv_4)
     mask_2 = MaxPoolingMask2D(pool_size)(conv_4)
 
-    conv_5 = Convolution2d(256, (kernel, kernel), padding="same")(pool_2)
-    conv_5 = Batchnormalization()(conv_5)
+    conv_5 = Convolution2D(256, (kernel, kernel), padding="same")(pool_2)
+    conv_5 = BatchNormalization()(conv_5)
     conv_5 = Activation("relu")(conv_5)
-    conv_6 = Convolution2d(256, (kernel, kernel), padding="same")(conv_5)
-    conv_6 = Batchnormalization()(conv_6)
+    conv_6 = Convolution2D(256, (kernel, kernel), padding="same")(conv_5)
+    conv_6 = BatchNormalization()(conv_6)
     conv_6 = Activation("relu")(conv_6)
-    conv_7 = Convolution2d(256, (kernel, kernel), padding="same")(conv_6)
-    conv_7 = Batchnormalization()(conv_7)
+    conv_7 = Convolution2D(256, (kernel, kernel), padding="same")(conv_6)
+    conv_7 = BatchNormalization()(conv_7)
     conv_7 = Activation("relu")(conv_7)
 
     pool_3 = MaxPooling2D(pool_size)(conv_7)
     mask_3 = MaxPoolingMask2D(pool_size)(conv_7)
 
-    conv_8 = Convolution2d(512, (kernel, kernel), padding="same")(pool_3)
-    conv_8 = Batchnormalization()(conv_8)
+    conv_8 = Convolution2D(512, (kernel, kernel), padding="same")(pool_3)
+    conv_8 = BatchNormalization()(conv_8)
     conv_8 = Activation("relu")(conv_8)
-    conv_9 = Convolution2d(512, (kernel, kernel), padding="same")(conv_8)
-    conv_9 = Batchnormalization()(conv_9)
+    conv_9 = Convolution2D(512, (kernel, kernel), padding="same")(conv_8)
+    conv_9 = BatchNormalization()(conv_9)
     conv_9 = Activation("relu")(conv_9)
-    conv_10 = Convolution2d(512, (kernel, kernel), padding="same")(conv_9)
-    conv_10 = Batchnormalization()(conv_10)
+    conv_10 = Convolution2D(512, (kernel, kernel), padding="same")(conv_9)
+    conv_10 = BatchNormalization()(conv_10)
     conv_10 = Activation("relu")(conv_10)
 
     pool_4 = MaxPooling2D(pool_size)(conv_10)
     mask_4 = MaxPoolingMask2D(pool_size)(conv_10)
 
-    conv_11 = Convolution2d(512, (kernel, kernel), padding="same")(pool_4)
-    conv_11 = Batchnormalization()(conv_11)
+    conv_11 = Convolution2D(512, (kernel, kernel), padding="same")(pool_4)
+    conv_11 = BatchNormalization()(conv_11)
     conv_11 = Activation("relu")(conv_11)
-    conv_12 = Convolution2d(512, (kernel, kernel), padding="same")(conv_11)
-    conv_12 = Batchnormalization()(conv_12)
+    conv_12 = Convolution2D(512, (kernel, kernel), padding="same")(conv_11)
+    conv_12 = BatchNormalization()(conv_12)
     conv_12 = Activation("relu")(conv_12)
-    conv_13 = Convolution2d(512, (kernel, kernel), padding="same")(conv_12)
-    conv_13 = Batchnormalization()(conv_13)
+    conv_13 = Convolution2D(512, (kernel, kernel), padding="same")(conv_12)
+    conv_13 = BatchNormalization()(conv_13)
     conv_13 = Activation("relu")(conv_13)
 
     pool_5 = MaxPooling2D(pool_size)(conv_13)
     mask_5 = MaxPoolingMask2D(pool_size)(conv_13)
+    print("Build enceder done..")
 
     # decoder
-    unpool_1 = UnPooling2DIndices()([pool_5, mask_5])
+    upsample_1 = UpSampling2D(pool_size)(pool_5)
+    unpool_1 = Multiply()([upsample_1, mask_5])
 
-    conv_14 = Convolution2d(512, (kernel, kernel), padding="same")(unpool_1)
-    conv_14 = Batchnormalization()(conv_14)
+    conv_14 = Convolution2D(512, (kernel, kernel), padding="same")(unpool_1)
+    conv_14 = BatchNormalization()(conv_14)
     conv_14 = Activation("relu")(conv_14)
-    conv_15 = Convolution2d(512, (kernel, kernel), padding="same")(conv_14)
-    conv_15 = Batchnormalization()(conv_15)
+    conv_15 = Convolution2D(512, (kernel, kernel), padding="same")(conv_14)
+    conv_15 = BatchNormalization()(conv_15)
     conv_15 = Activation("relu")(conv_15)
-    conv_16 = Convolution2d(512, (kernel, kernel), padding="same")(conv_15)
-    conv_16 = Batchnormalization()(conv_16)
+    conv_16 = Convolution2D(512, (kernel, kernel), padding="same")(conv_15)
+    conv_16 = BatchNormalization()(conv_16)
     conv_16 = Activation("relu")(conv_16)
 
-    unpool_2 = UnPooling2DIndices()([conv_16, mask_4])
+    upsample_2 = UpSampling2D(pool_size)(conv_16)
+    unpool_2 = Multiply()([upsample_2, mask_4])
 
-    conv_17 = Convolution2d(512, (kernel, kernel), padding="same")(unpool_2)
-    conv_17 = Batchnormalization()(conv_17)
+    conv_17 = Convolution2D(512, (kernel, kernel), padding="same")(unpool_2)
+    conv_17 = BatchNormalization()(conv_17)
     conv_17 = Activation("relu")(conv_17)
-    conv_18 = Convolution2d(512, (kernel, kernel), padding="same")(conv_17)
-    conv_18 = Batchnormalization()(conv_18)
+    conv_18 = Convolution2D(512, (kernel, kernel), padding="same")(conv_17)
+    conv_18 = BatchNormalization()(conv_18)
     conv_18 = Activation("relu")(conv_18)
-    conv_19 = Convolution2d(256, (kernel, kernel), padding="same")(conv_18)
-    conv_19 = Batchnormalization()(conv_19)
+    conv_19 = Convolution2D(256, (kernel, kernel), padding="same")(conv_18)
+    conv_19 = BatchNormalization()(conv_19)
     conv_19 = Activation("relu")(conv_19)
 
-    unpool_3 = UnPooling2DIndices()([conv_19, mask_3])
+    upsample_3 = UpSampling2D(pool_size)(conv_19)
+    unpool_3 = Multiply()([upsample_3, mask_3])
 
-    conv_20 = Convolution2d(256, (kernel, kernel), padding="same")(unpool_3)
-    conv_20 = Batchnormalization()(conv_20)
+    conv_20 = Convolution2D(256, (kernel, kernel), padding="same")(unpool_3)
+    conv_20 = BatchNormalization()(conv_20)
     conv_20 = Activation("relu")(conv_20)
-    conv_21 = Convolution2d(256, (kernel, kernel), padding="same")(conv_20)
-    conv_21 = Batchnormalization()(conv_21)
+    conv_21 = Convolution2D(256, (kernel, kernel), padding="same")(conv_20)
+    conv_21 = BatchNormalization()(conv_21)
     conv_21 = Activation("relu")(conv_21)
-    conv_22 = Convolution2d(128, (kernel, kernel), padding="same")(conv_21)
-    conv_22 = Batchnormalization()(conv_22)
+    conv_22 = Convolution2D(128, (kernel, kernel), padding="same")(conv_21)
+    conv_22 = BatchNormalization()(conv_22)
     conv_22 = Activation("relu")(conv_22)
 
-    unpool_4 = UnPooling2DIndices()([conv_22, mask_2])
+    upsample_4 = UpSampling2D(pool_size)(conv_22)
+    unpool_4 = Multiply()([upsample_4, mask_2])
 
-    conv_23 = Convolution2d(128, (kernel, kernel), padding="same")(unpool_4)
-    conv_23 = Batchnormalization()(conv_23)
+    conv_23 = Convolution2D(128, (kernel, kernel), padding="same")(unpool_4)
+    conv_23 = BatchNormalization()(conv_23)
     conv_23 = Activation("relu")(conv_23)
-    conv_24 = Convolution2d(64, (kernel, kernel), padding="same")(conv_23)
-    conv_24 = Batchnormalization()(conv_24)
+    conv_24 = Convolution2D(64, (kernel, kernel), padding="same")(conv_23)
+    conv_24 = BatchNormalization()(conv_24)
     conv_24 = Activation("relu")(conv_24)
 
-    unpool_5 = UnPooling2DIndices()([conv_24, mask_1])
+    upsample_5 = UpSampling2D(pool_size)(conv_24)
+    unpool_5 = Multiply()([upsample_5, mask_1])
 
-    conv_25 = Convolution2d(64, (kernel, kernel), padding="same")(unpool_5)
-    conv_25 = Batchnormalization()(conv_25)
+    conv_25 = Convolution2D(64, (kernel, kernel), padding="same")(unpool_5)
+    conv_25 = BatchNormalization()(conv_25)
     conv_25 = Activation("relu")(conv_25)
 
     conv_26 = Convolution2D(n_labels, (1, 1), padding="valid")(conv_25)
     conv_26 = BatchNormalization()(conv_26)
 
     conv_26 = Reshape((input_shape[0] * input_shape[1], n_labels), input_shape=(input_shape[0], input_shape[1], n_labels))(conv_26)
-    conv_26 = Permute(2, 1)(outputs)
+    conv_26 = Permute((2, 1))(conv_26)
     predictions = Activation("softmax")(conv_26)
+    print("Build decoder done..")
 
     segnet = Model(inputs=inputs, outputs=predictions, name="SegNet")
 
     return segnet
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("")
-    parser.add_argument("",default="",help="")
-    parser.add_argument("",default = ,help="Input image size")
-    parser.add_argument("",type=int,default=,
-                      help="")
-    parser.add_argument("", type=int,default=,
-                      help="")
+    parser.add_argument("--input_shape", default=(256, 256, 3),help="Input images shape")
+    parser.add_argument("--n_labels", default=10, type=int, help="Number of label")
+    parser.add_argument("--kernel", default=3, type=int, help="Kernel size")
+    parser.add_argument("--pool_size", default=(2, 2), help="")
     args = parser.parse_args()
 
+    segnet = CreateSegNet(args.input_shape, args.n_labels, args.kernel, args.pool_size)
+
+    with open("SegNet_model.json", "w") as json_file:
+        json_file.write(json.dumps(json.loads(segnet.to_json()), indent=2))
+    print("Save json model done...")
--- keras_IndicesPooling.py
+++ keras_IndicesPooling.py
@@ -5,204 +5,26 @@ from keras.layers.core import Layer
 from keras.layers.merge import Multiply, Concatenate
 import numpy as np
 
-class _Merge(Layer):
-    def __init__(self, **kwargs):
-        super(_Merge, self).__init__(**kwargs)
-        self.supports_masking = True
 
+class MaxPoolingMask2D(Layer):
+    def __init__(self, output_dim, **kwargs):
+        self.output_dim = output_dim
+        super(MaxPoolingMask2D, self).__init__(**kwargs)
 
-    def _merge_function(self, inputs):
-        raise NotImplementedError
-
-
-    def _compute_elemwise_op_output_shape(self, shape1, shape2):
-        if None in [shape1, shape2]:
-            return None
-        elif len(shape1) < len(shape2):
-            return self._compute_elemwise_op_output_shape(shape2, shape1)
-        elif len(shape2) == 0:
-            return shape1
-        output_shape = list(shape1[:-len(shape2)])
-        for i, j in zip(shape1[-len(shape2):], shape2):
-            if i is None or j is None:
-                output_shape.append(None)
-            elif i == 1:
-                output_shape.append(j)
-            elif j == 1:
-                output_shape.append(i)
-            else:
-                if i != j:
-                    raise ValueError('Operands could not be broadcast '
-                                     'together with shapes ' +
-                                     str(shape1) + ' ' + str(shape2))
-                output_shape.append(i)
-        return tuple(output_shape)
-
-
-    def build(self, input_shape):
-        # Used purely for shape validation.
-        if not isinstance(input_shape, list):
-            raise ValueError('A merge layer should be called '
-                             'on a list of inputs.')
-        if len(input_shape) < 2:
-            raise ValueError('A merge layer should be called '
-                             'on a list of at least 2 inputs. '
-                             'Got ' + str(len(input_shape)) + ' inputs.')
-        batch_sizes = [s[0] for s in input_shape if s is not None]
-        batch_sizes = set(batch_sizes)
-        batch_sizes -= set([None])
-        if len(batch_sizes) > 1:
-            raise ValueError('Can not merge tensors with different '
-                             'batch sizes. Got tensors with shapes : ' +
-                             str(input_shape))
-        if input_shape[0] is None:
-            output_shape = None
-        else:
-            output_shape = input_shape[0][1:]
-        for i in range(1, len(input_shape)):
-            if input_shape[i] is None:
-                shape = None
-            else:
-                shape = input_shape[i][1:]
-            output_shape = self._compute_elemwise_op_output_shape(output_shape, shape)
-        # If the inputs have different ranks, we have to reshape them
-        # to make them broadcastable.
-        if None not in input_shape and len(set(map(len, input_shape))) == 1:
-            self._reshape_required = False
-        else:
-            self._reshape_required = True
-
-
-    def call(self, inputs):
-        if self._reshape_required:
-            reshaped_inputs = []
-            input_ndims = list(map(K.ndim, inputs))
-            if None not in input_ndims:
-                # If ranks of all inputs are available,
-                # we simply expand each of them at axis=1
-                # until all of them have the same rank.
-                max_ndim = max(input_ndims)
-                for x in inputs:
-                    x_ndim = K.ndim(x)
-                    for _ in range(max_ndim - x_ndim):
-                        x = K.expand_dims(x, 1)
-                    reshaped_inputs.append(x)
-                return self._merge_function(reshaped_inputs)
-            else:
-                # Transpose all inputs so that batch size is the last dimension.
-                # (batch_size, dim1, dim2, ... ) -> (dim1, dim2, ... , batch_size)
-                transposed = False
-                for x in inputs:
-                    x_ndim = K.ndim(x)
-                    if x_ndim is None:
-                        x_shape = K.shape(x)
-                        batch_size = x_shape[0]
-                        new_shape = K.concatenate([x_shape[1:], K.expand_dims(batch_size)])
-                        x_transposed = K.reshape(x, K.stack([batch_size, K.prod(x_shape[1:])]))
-                        x_transposed = K.permute_dimensions(x_transposed, (1, 0))
-                        x_transposed = K.reshape(x_transposed, new_shape)
-                        reshaped_inputs.append(x_transposed)
-                        transposed = True
-                    elif x_ndim > 1:
-                        dims = list(range(1, x_ndim)) + [0]
-                        reshaped_inputs.append(K.permute_dimensions(x, dims))
-                        transposed = True
-                    else:
-                        # We don't transpose inputs if they are 1D vectors or scalars.
-                        reshaped_inputs.append(x)
-                y = self._merge_function(reshaped_inputs)
-                y_ndim = K.ndim(y)
-                if transposed:
-                    # If inputs have been transposed, we have to transpose the output too.
-                    if y_ndim is None:
-                        y_shape = K.shape(y)
-                        y_ndim = K.shape(y_shape)[0]
-                        batch_size = y_shape[y_ndim - 1]
-                        new_shape = K.concatenate([K.expand_dims(batch_size), y_shape[:y_ndim - 1]])
-                        y = K.reshape(y, (-1, batch_size))
-                        y = K.permute_dimensions(y, (1, 0))
-                        y = K.reshape(y, new_shape)
-                    elif y_ndim > 1:
-                        dims = [y_ndim - 1] + list(range(y_ndim - 1))
-                        y = K.permute_dimensions(y, dims)
-                return y
-        else:
-            return self._merge_function(inputs)
-
+    def call(self, x):
+        orig = x
+        x = MaxPooling2D()(x)
+        x = UpSampling2D()(x)
+        mask = K.tf.equal(orig, x)
+        assert mask.get_shape().as_list() == orig.get_shape().as_list()
+        return K.cast(mask, dtype="float32")
 
     def compute_output_shape(self, input_shape):
-        if input_shape[0] is None:
-            output_shape = None
-        else:
-            output_shape = input_shape[0][1:]
-        for i in range(1, len(input_shape)):
-            if input_shape[i] is None:
-                shape = None
-            else:
-                shape = input_shape[i][1:]
-            output_shape = self._compute_elemwise_op_output_shape(output_shape, shape)
-        batch_sizes = [s[0] for s in input_shape if s is not None]
-        batch_sizes = set(batch_sizes)
-        batch_sizes -= set([None])
-        if len(batch_sizes) == 1:
-            output_shape = (list(batch_sizes)[0],) + output_shape
-        else:
-            output_shape = (None,) + output_shape
-        return output_shape
-
-
-    def compute_mask(self, inputs, mask=None):
-        if mask is None:
-            return None
-        if not isinstance(mask, list):
-            raise ValueError('`mask` should be a list.')
-        if not isinstance(inputs, list):
-            raise ValueError('`inputs` should be a list.')
-        if len(mask) != len(inputs):
-            raise ValueError('The lists `inputs` and `mask` '
-                             'should have the same length.')
-        if all([m is None for m in mask]):
-            return None
-        masks = [K.expand_dims(m, 0) for m in mask if m is not None]
-        return K.all(K.concatenate(masks, axis=0), axis=0, keepdims=False)
-
-# UnPooling2D layey:input feature maps and mask
-class UnPooling2DIndices(_Merge):
-    def _merge_function(self, inputs):
-        x = inputs[0]
-        mask = inputs[1]
-
-        # Decision UpSampling size
-        mask_shape = mask.get_shape().as_list()
-        x_shape = x.get_shape().as_list()
-        pool_size = (mask_shape[1] / x_shape[1], mask_shape[2] / x_shape[2])
-
-        x = UpSampling2D(size=pool_size)(x)
-        return Multiply()([mask, x])
-
-
-# Maxpooling with argmax indices
-class MaxPoolingMask2D(MaxPooling2D):
-    def __init__(self, pool_size=(2, 2), strides=None, padding='same',
-                 dim_ordering='default', **kwargs):
-        super(MaxPoolingMask2D, self).__init__(pool_size, strides, padding,
-                                           dim_ordering, **kwargs)
-
-    def _pooling_function(self, inputs, pool_size, strides,
-                          padding, dim_ordering):
-        orig = inputs
-        x = K.pool2d(inputs, pool_size, strides, border_mode,
-                 dim_ordering, pool_mode='max')
-        x = UpSampling2D(size=pool_size)(pooled)
-
-        xReshaped = Reshape((1,channels_after_conv_block, h_after, w_after))(x)
-        origReshaped = Reshape((1,channels_after_conv_block, h_after, w_after))(orig)
-
-        together = Concatenate(axis=1)([origReshaped,xReshaped])
-
-        bool_mask = Lambda(lambda t: K.greater_equal(t[:,0], t[:,1]),output_shape=(channels_after_conv_block, h_after, w_after))(together)
-        mask = Lambda(lambda t: K.cast(t, dtype='float32'))(bool_mask)
-        return mask
-
-    def get_output_shape_for(self, input_shape):
         return input_shape
+
+# future implement
+"""
+class UnPooling2DIndices(Layer):
+    def __init__
+
+"""
