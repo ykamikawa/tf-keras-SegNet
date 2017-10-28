0a254b019caaa5c1d955d70ac13804437686e6f1
ykamikawa <ytkmkw@gmail.com>
2017-10-28 17:30:16 +0900

    Fixed Mylayers

--- SegNet.py
+++ SegNet.py
@@ -7,6 +7,7 @@ from keras.layers.merge import Multiply, Concatenate
 from keras.utils import np_utils
 
 from keras_IndicesPooling import MaxPoolingMask2D
+from Mylayers import MaxPoolingWithArgmax2D, MaxUnpooling2D
 
 import os
 import numpy as np
@@ -15,7 +16,6 @@ import json
 from PIL import Image
 
 def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
-
     # encoder
     inputs = Input(shape=input_shape)
 
@@ -25,9 +25,11 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     conv_2 = Convolution2D(64, (kernel, kernel), padding="same")(conv_1)
     conv_2 = BatchNormalization()(conv_2)
     conv_2 = Activation("relu")(conv_2)
-
+    """
     pool_1 = MaxPooling2D(pool_size)(conv_2)
     mask_1 = MaxPoolingMask2D(pool_size)(conv_2)
+    """
+    pool_1, mask_1 = MaxPoolingWithArgmax2D(pool_size)(conv_2)
 
     conv_3 = Convolution2D(128, (kernel, kernel), padding="same")(pool_1)
     conv_3 = BatchNormalization()(conv_3)
@@ -35,9 +37,11 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     conv_4 = Convolution2D(128, (kernel, kernel), padding="same")(conv_3)
     conv_4 = BatchNormalization()(conv_4)
     conv_4 = Activation("relu")(conv_4)
-
+    """
     pool_2 = MaxPooling2D(pool_size)(conv_4)
     mask_2 = MaxPoolingMask2D(pool_size)(conv_4)
+    """
+    pool_2, mask_2 = MaxPoolingWithArgmax2D(pool_size)(conv_4)
 
     conv_5 = Convolution2D(256, (kernel, kernel), padding="same")(pool_2)
     conv_5 = BatchNormalization()(conv_5)
@@ -48,9 +52,11 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     conv_7 = Convolution2D(256, (kernel, kernel), padding="same")(conv_6)
     conv_7 = BatchNormalization()(conv_7)
     conv_7 = Activation("relu")(conv_7)
-
+    """
     pool_3 = MaxPooling2D(pool_size)(conv_7)
     mask_3 = MaxPoolingMask2D(pool_size)(conv_7)
+    """
+    pool_3, mask_3 = MaxPoolingWithArgmax2D(pool_size)(conv_7)
 
     conv_8 = Convolution2D(512, (kernel, kernel), padding="same")(pool_3)
     conv_8 = BatchNormalization()(conv_8)
@@ -61,9 +67,11 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     conv_10 = Convolution2D(512, (kernel, kernel), padding="same")(conv_9)
     conv_10 = BatchNormalization()(conv_10)
     conv_10 = Activation("relu")(conv_10)
-
+    """
     pool_4 = MaxPooling2D(pool_size)(conv_10)
     mask_4 = MaxPoolingMask2D(pool_size)(conv_10)
+    """
+    pool_4, mask_4 = MaxPoolingWithArgmax2D(pool_size)(conv_10)
 
     conv_11 = Convolution2D(512, (kernel, kernel), padding="same")(pool_4)
     conv_11 = BatchNormalization()(conv_11)
@@ -74,14 +82,19 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     conv_13 = Convolution2D(512, (kernel, kernel), padding="same")(conv_12)
     conv_13 = BatchNormalization()(conv_13)
     conv_13 = Activation("relu")(conv_13)
-
+    """
     pool_5 = MaxPooling2D(pool_size)(conv_13)
     mask_5 = MaxPoolingMask2D(pool_size)(conv_13)
+    """
+    pool_5, mask_5 = MaxPoolingWithArgmax2D(pool_size)(conv_13)
     print("Build enceder done..")
 
     # decoder
+    """
     upsample_1 = UpSampling2D(pool_size)(pool_5)
     unpool_1 = Multiply()([upsample_1, mask_5])
+    """
+    unpool_1 = MaxUnpooling2D(pool_size)([pool_5, mask_5])
 
     conv_14 = Convolution2D(512, (kernel, kernel), padding="same")(unpool_1)
     conv_14 = BatchNormalization()(conv_14)
@@ -92,9 +105,11 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     conv_16 = Convolution2D(512, (kernel, kernel), padding="same")(conv_15)
     conv_16 = BatchNormalization()(conv_16)
     conv_16 = Activation("relu")(conv_16)
-
+    """
     upsample_2 = UpSampling2D(pool_size)(conv_16)
     unpool_2 = Multiply()([upsample_2, mask_4])
+    """
+    unpool_2 = MaxUnpooling2D(pool_size)([conv_16, mask_4])
 
     conv_17 = Convolution2D(512, (kernel, kernel), padding="same")(unpool_2)
     conv_17 = BatchNormalization()(conv_17)
@@ -105,9 +120,11 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     conv_19 = Convolution2D(256, (kernel, kernel), padding="same")(conv_18)
     conv_19 = BatchNormalization()(conv_19)
     conv_19 = Activation("relu")(conv_19)
-
+    """
     upsample_3 = UpSampling2D(pool_size)(conv_19)
     unpool_3 = Multiply()([upsample_3, mask_3])
+    """
+    unpool_3 = MaxUnpooling2D(pool_size)([conv_19, mask_3])
 
     conv_20 = Convolution2D(256, (kernel, kernel), padding="same")(unpool_3)
     conv_20 = BatchNormalization()(conv_20)
@@ -118,9 +135,11 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     conv_22 = Convolution2D(128, (kernel, kernel), padding="same")(conv_21)
     conv_22 = BatchNormalization()(conv_22)
     conv_22 = Activation("relu")(conv_22)
-
+    """
     upsample_4 = UpSampling2D(pool_size)(conv_22)
     unpool_4 = Multiply()([upsample_4, mask_2])
+    """
+    unpool_4 = MaxUnpooling2D(pool_size)([conv_22, mask_2])
 
     conv_23 = Convolution2D(128, (kernel, kernel), padding="same")(unpool_4)
     conv_23 = BatchNormalization()(conv_23)
@@ -128,9 +147,11 @@ def CreateSegNet(input_shape, n_labels, kernel=3, pool_size=(2, 2)):
     conv_24 = Convolution2D(64, (kernel, kernel), padding="same")(conv_23)
     conv_24 = BatchNormalization()(conv_24)
     conv_24 = Activation("relu")(conv_24)
-
+    """
     upsample_5 = UpSampling2D(pool_size)(conv_24)
     unpool_5 = Multiply()([upsample_5, mask_1])
+    """
+    unpool_5 = MaxUnpooling2D(pool_size)([conv_24, mask_1])
 
     conv_25 = Convolution2D(64, (kernel, kernel), padding="same")(unpool_5)
     conv_25 = BatchNormalization()(conv_25)
@@ -157,6 +178,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     segnet = CreateSegNet(args.input_shape, args.n_labels, args.kernel, args.pool_size)
+    print(segnet.summary())
 
     with open("SegNet_model.json", "w") as json_file:
         json_file.write(json.dumps(json.loads(segnet.to_json()), indent=2))
